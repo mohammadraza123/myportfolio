2f89fcbb62aeec8f12f38b01360a9fd3e6c96692
Muhammad Raza <mohammadrazaarain10@gmail.com>
2025-03-23 19:57:54 +0500

    all changes

---
+++ src/portfolio.js
@@ -0,0 +1,411 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Mohammad Raza",
+  title: "Hi, it's Mohammad Raza",
+  subTitle: emoji(
+    "A passionate Frontend Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs ."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1I9wxE4XVw4sUTP5xSofXCrxTE03YhreD/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/mohammadraza123",
+  linkedin: "https://www.linkedin.com/in/mohammad-raza-2210b9243/",
+  gmail: "mohammadrazaarain10@gmail.com",
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FRONTEND DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("⚡ Integration of third party services such as Firebase")
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "Html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "Css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "Sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "Reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "Firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Intermediate",
+      logo: require("./assets/images/college.jpg"),
+      subHeader: "In Computer Science",
+      duration: "2023",
+      descBullets: [
+        "Gained foundational knowledge in programming, algorithms, and data structures.",
+        "Hands-on experience with problem-solving, software development, and computer systems."
+      ]
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend (React)", //Insert stack or technology you have experience in
+      progressPercentage: "80%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "JavaScript Developer",
+      company: "IT Panda",
+      companylogo: require("./assets/images/itpanda.jpg"),
+      date: "June 2023 – October 2023",
+      descBullets: [
+        "Completed a 4-month internship at IT Panda, focusing on frontend development using JavaScript and React.",
+        "Worked on diverse projects, contributing to real-world web development solutions.",
+        "Enhanced web development skills and gained hands-on experience with modern technologies."
+      ]
+    },
+    {
+      role: "Reactjs Developer",
+      company: "Rayon Studios",
+      companylogo: require("./assets/images/rayon.png"),
+      date: "December 2023 – Present",
+      descBullets: [
+        "Individually designed and developed complete websites with a focus on mobile responsiveness.",
+        "Built and implemented core functionalities and application logic using React and JavaScript.",
+        "Gained practical experience as a Junior React/JavaScript Developer, contributing to seamless user experiences."
+      ]
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME OF THE PROJECTS OF MY CAREER",
+  projects: [
+   
+    {
+      image: require("./assets/images/olx.png"),
+      projectName: "Olx (CLone)",
+
+      projectDesc: [
+        {
+          description:
+            "I created an OLX Clone project using React, Firebase, Redux, and React Router. This project involved integrating Firebase for user authentication and data storage."
+        },
+        {
+          description:
+            "Managed global state with Redux and implemented dynamic routing for smooth navigation between different pages."
+        },
+        {
+          description:
+            "The project is available on GitHub, showcasing my ability to build a full-stack web application with modern tools."
+        }
+      ],
+
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://olx-ecommerce.vercel.app/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/turo.png"),
+      projectName: "Turo Website",
+
+      projectDesc: [
+        {
+          description:
+            "Developed a Turo website clone using React, replicating the core functionality and user interface of the platform."
+        },
+        {
+          description:
+            "Ensured full responsiveness for mobile devices, optimizing the user experience across different screen sizes."
+        },
+        {
+          description:
+            "Utilized React and Ant Design (AntD) for building modern, clean, and responsive UI components."
+        }
+      ],
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://olx-ecommerce.vercel.app/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/weather.png"),
+      projectName: "Weather App",
+
+      projectDesc: [
+        {
+          description:
+            "Designed and built a user-friendly interface for real-time weather updates."
+        },
+        {
+          description:
+            "Utilized weather APIs to fetch and display current weather data for cities and countries worldwide."
+        },
+        {
+          description:
+            "Implemented responsive design and seamless API integration for accurate and quick weather insights."
+        }
+      ],
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://weather-app-raza.netlify.app/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !"
+
+  // achievementsCards: [
+  //   {
+  //     title: "Google Code-In Finalist",
+  //     subtitle:
+  //       "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+  //     image: require("./assets/images/codeInLogo.webp"),
+  //     imageAlt: "Google Code-In Logo",
+  //     footerLink: [
+  //       {
+  //         name: "Certification",
+  //         url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+  //       },
+  //       {
+  //         name: "Award Letter",
+  //         url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+  //       },
+  //       {
+  //         name: "Google Code-in Blog",
+  //         url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     title: "Google Assistant Action",
+  //     subtitle:
+  //       "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+  //     image: require("./assets/images/googleAssistantLogo.webp"),
+  //     imageAlt: "Google Assistant Action Logo",
+  //     footerLink: [
+  //       {
+  //         name: "View Google Assistant Action",
+  //         url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+  //       }
+  //     ]
+  //   },
+
+  //   {
+  //     title: "PWA Web App Developer",
+  //     subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+  //     image: require("./assets/images/pwaLogo.webp"),
+  //     imageAlt: "PWA Logo",
+  //     footerLink: [
+  //       {name: "Certification", url: ""},
+  //       {
+  //         name: "Final Project",
+  //         url: "https://pakistan-olx-1.firebaseapp.com/"
+  //       }
+  //     ]
+  //   }
+  // ],
+  // display: true // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  // blogs: [
+  //   {
+  //     url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+  //     title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+  //     description:
+  //       "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+  //   },
+  //   {
+  //     url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+  //     title: "Why REACT is The Best?",
+  //     description:
+  //       "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+  //   }
+  // ],
+  // display: true // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  // talks: [
+  //   {
+  //     title: "Build Actions For Google Assistant",
+  //     subtitle: "Codelab at GDG DevFest Karachi 2019",
+  //     slides_url: "https://bit.ly/saadpasta-slides",
+  //     event_url: "https://www.facebook.com/events/2339906106275053/"
+  //   }
+  // ],
+  // display: true // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Resume Section
+const resumeSection = {
+  title: "Resume",
+  subtitle: "Feel free to download my resume",
+
+  // Please Provide with Your Podcast embeded Link
+  display: true // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Reach out me via",
+  number: "+92-3068565775",
+  email_address: "mohammadrazaarain10@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: true // Set true to display this section, defaults to false
+};
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable,
+  resumeSection
+};
